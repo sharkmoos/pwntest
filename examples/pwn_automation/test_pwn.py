e272ece8ea574263a4f0ce97d37a7e1abec6ea27
sharkmoos <muddy117@gmail.com>
2023-02-14 12:29:23 +0000

    testing ci/cd

--- examples/pwn_automation/test_pwn.py
+++ examples/pwn_automation/test_pwn.py
@@ -1,12 +1,12 @@
 import sys
 import unittest
-from exploit import get_flag as exploit_flag  
+from exploit import get_flag as exploit_flag
 from exploit import get_shell as exploit_shell
 
 sys.path.append("/mnt/c/Users/Muddy/OneDrive/Uni/Dissertation/pwntest/")
 import pwntest
 
-tester = pwntest.PwnTest("127.0.0.1", 9001, binary_path="examples/pwn_remote_flag/challenge/challenge")
+tester = pwntest.PwnTest("127.0.0.1", 9001, binary_path="examples/pwn_automation/challenge/challenge")
 
 
 def test_remote_flag():
