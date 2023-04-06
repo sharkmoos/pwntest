383b2b682db0fd8defaf257a7e3e4a606c68b970
sharkmoos <muddy117@gmail.com>
2023-04-06 10:38:15 +0100

    Updated docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
 # It seems to skip the files if we don't delete them
-rm docs/source/binary.rst
-rm docs/source/web.rst
-rm docs/source/extended_gdb.rst
-rm docs/source/pwntest.rst
+#rm build_docs/source/binary.rst
+#rm build_docs/source/web.rst
+#rm build_docs/source/extended_gdb.rst
+#rm build_docs/source/pwntest.rst
 
 # Why is there no recursive option?
-sphinx-apidoc -o docs/source pwntest/
-sphinx-apidoc -o docs/source pwntest/modules/
+sphinx-apidoc -o build_docs/source pwntest/
+sphinx-apidoc -o build_docs/source pwntest/modules/
 
-# this is the same as pwntools, so no need to document it
-rm docs/source/gdb_api_bridge.rst
-
-cd docs || exit 1
+cd build_docs || exit 1
 make html
